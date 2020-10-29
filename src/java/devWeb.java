914b8273d6b590732d21399b088b1a08a094802d
Shroudmaster <hugofmealt@gmail.com>
2020-10-29 01:58:26 -0300

    Build linda
    -Links do dashboard funciona
    -Tem um servlet no login!!

--- src/java/devWeb.java
+++ src/java/devWeb.java
@@ -1,87 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author hugof
- */
-@WebServlet(urlPatterns = {"/devWeb"})
+@WebServlet(name = "devWeb", urlPatterns = {"/devWeb"})
 public class devWeb extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet devWeb</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet devWeb at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+      
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        // pegando os parâmetros do request
+        String cpf = request.getParameter("cpf");
+        String senha = request.getParameter("senha");
+
+        if (cpf.isEmpty() || senha.isEmpty()) {
+            // voltando para página inicial
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Resutado</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Dados nao entrados</h1>");
+            out.println("<a href = 'index.html'>Voltar a página inicial</a>");
+            out.println("</body>");
+            out.println("</html>");
+            
+        } else {
+            // gerando a resposta
+            response.sendRedirect("admin/index.html");
+            
+        }
+
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
